ea81c30388b233d9776aa70a34e44f397e8b81c9
Robert Surujbhan <38476630+rsurujbhan@users.noreply.github.com>
2020-07-15 16:22:47 -0700

    Create JetEmpTableDataListener.js

---
+++ AgentBrowserUI/Extensions/JetEmpTableDataListener.js
@@ -0,0 +1,69 @@
+
+function addTableHandler() {
+
+    ORACLE_SERVICE_CLOUD.extension_loader.load("JetEmpsVirtualTable", "1.0").then(function (extensionProvider) {
+
+        extensionProvider.registerAnalyticsExtension(function (analyticsContext) {
+
+            analyticsContext.addTableDataRequestListener('OracleJET$Emps', function (report) {
+            
+                var dataHandlerPromise = new ORACLE_SERVICE_CLOUD.ExtensionPromise();
+
+                getExternalData().then(function (data) {
+                    var tableData = [];
+                    tableData = data.items;
+                    var recordCount = tableData.length;
+                    var reportData = report.createReportData();
+                    var columnDefinitionList = report.getReportDefinition().getColumnDefinitions();
+
+                    reportData.setTotalRecordCount(recordCount);
+
+                    for (var i = 0; i < tableData.length; i++) {
+                        var row = report.createReportDataRow();
+                        //console.log(tableData[i]);
+                        var itemData = tableData[i];
+                        //console.log(itemData);
+                        window.parent.window.$.each(columnDefinitionList, function(i, obj) {
+                            var idx = obj["columnReference"].replace("OracleJET$Emps.", "");
+                            //console.log("idx", i, idx);
+                            var data = itemData[idx];
+                            //console.log(idx, data);
+                            var cellData = itemData[idx];
+                            var cell = report.createReportDataCell();
+                            cell.setData(cellData);
+                            row.cells.push(cell);
+                        });
+                        reportData.rows.push(row);
+                    }
+                    //console.log(reportData);
+                    dataHandlerPromise.resolve(reportData);
+                }).catch(function (error) {
+                    //console.log(error);
+                    dataHandlerPromise.reject(error);
+                }); //getExternalData
+
+                return dataHandlerPromise;
+            });
+        });
+    });
+}
+
+function getExternalData() {
+    var apiEndpoint = "https://apex.oracle.com/pls/apex/oraclejet/emp/";
+    return new ORACLE_SERVICE_CLOUD.ExtensionPromise((resolve, reject) => {
+        window.parent.window.$.ajax({
+            url: apiEndpoint,
+            method: "GET",
+            dataType: "json",
+            success: function (data) {
+                resolve(data);
+            },
+            error: function (error) {
+                reject(error);
+            }
+        });
+    });
+}
+
+// initialize
+addTableHandler();
